dc5f6ba3b4ae81ae9ddee2984c9ae0a61cfdc601
nicholasp13 <111471861+nicholasp13@users.noreply.github.com>
2024-11-22 16:09:34 -0800

    Added lua script to run own projects on Visual Studios

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "pugixml"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+    staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.hpp",
+		"src/**.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
